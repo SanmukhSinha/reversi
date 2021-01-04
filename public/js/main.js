ec2ca4564715cb5d4196314b921d93b43e4f9bcc
Sanmukh Sinha <sanmukh.sinha98@gmail.com>
2021-01-04 05:48:13 +0530

    Added chatroom

--- public/js/main.js
+++ public/js/main.js
@@ -17,4 +17,32 @@ var username = getURLParameters('username');
 if(typeof username == 'undefined' || !username){
     username = "Anonymous_" + Math.floor(Math.random() * 100 + 1);
 }
-document.getElementById('messages').insertAdjacentHTML('afterbegin', '<h4>'+ username +'</h4>');
+//document.getElementById('messages').insertAdjacentHTML('afterbegin', '<h4>'+ username +'</h4>');
+var chat_room = 'One_Room';
+
+
+/*Connect to the socket server*/
+
+var socket =  io.connect();
+
+socket.on('log', function(array){
+    console.log.apply(console,array);
+});
+
+socket.on('join_room_response',function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    document.getElementById('messages').insertAdjacentHTML('afterbegin', '<p>New User joined the room: '+ payload.username +'</p>');
+
+});
+
+document.addEventListener("DOMContentLoaded",function(event){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+
+    console.log('*** Client Log Message: \'join room\' payload: '+ JSON.stringify(payload));
+    socket.emit('join_room',payload);
+});
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/**********************************************/
+/*      Setup the static file server         */
+
 /*Including static file webserver and http server libs*/
 var nstatic = require('node-static');
 
@@ -28,3 +31,95 @@ var app = http.createServer(
         ).listen(port);
 
 console.log('The server is Running');
+
+/**********************************************/
+/*      Setup the websocket server         */
+var io = require('socket.io')(app);
+
+io.on('connection', function(socket){
+
+    function log(){
+        var array = ['***Server Log Messages: '];
+        for( var i = 0; i<arguments.length; i++){
+            array.push(arguments[i]);
+            console.log(arguments[i]);
+        }
+        socket.emit('log',array);
+        socket.broadcast.emit('log',array);
+    }
+    
+    log('A website connected to the server');
+
+    socket.on('disconnect', function(socket){
+        log('A website disconnected from the server');
+    });
+
+    /*payload: 
+        {   
+            'room': room to join,
+            'username': username of person joining
+        }
+      join_room_response:
+        {
+            'result': success,
+            'room': room joined,
+            'username': username that joined,
+            'membership': no. of people in the room
+        }
+        or
+        {
+            'result': fail,
+            'message': failure message
+        }
+    */
+
+    socket.on('join_room', function(payload){
+        log('server received a command','join_room',payload);
+        if((typeof payload === undefined )|| !payload){
+            var error_message = 'join_room had no room, aborted';
+            log(error_message);
+            socket.emit('join_room_response',{result: 'fail', message: error_message});
+            return;
+        }
+
+        var room = payload.room;
+        if((typeof room === undefined )|| !payload){
+            var error_message = 'join_room had no payload, aborted';
+            log(error_message);
+            socket.emit('join_room_response',{result: 'fail', message: error_message});
+            return;
+        }
+
+        var username = payload.username;
+        if((typeof username === undefined )|| !username){
+            var error_message = 'join_room had no username, aborted';
+            log(error_message);
+            socket.emit('join_room_response',{result: 'fail', message: error_message});
+            return;
+        }
+        
+        socket.join(room,function(){
+            console.log("Socket now in rooms", socket.rooms);
+        });
+        
+
+        var roomObject = io.sockets.adapter.rooms.get(room);
+        //console.log(roomObject);
+        if((typeof roomObject === undefined )|| !roomObject){
+            var error_message = 'join_room had no roomObject, aborted';
+            log(error_message);
+            socket.emit('join_room_response',{result: 'fail', message: error_message});
+            return;
+        }
+
+        var numClients = roomObject.size;
+        var success_data = {
+                            'result': 'success',
+                            'room': room,
+                            'username': username,
+                            'membership': numClients
+                            };
+        io.sockets.in(room).emit('join_room_response',success_data);
+        log('Room: ' + room + ' was just joined by '+ username);
+    });
+});
